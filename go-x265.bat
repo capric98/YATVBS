34ba861c3d9d313a6e1ebcf5a6ea4e3d4b20bc7f
HZH <42015599+capric98@users.noreply.github.com>
2020-03-22 15:46:57 +0800

    Update go-x265.bat

--- go-x265.bat
+++ go-x265.bat
@@ -1,6 +1,6 @@
 @echo off
 set FILE=%~dpn1
 
-vspipe --y4m "%1" - | x265 --y4m - --profile main10 --pools + --pmode --ctu 32 --min-cu-size 8 --tu-intra-depth 3 --tu-inter-depth 2 --limit-tu 2 --limit-refs 0 --rd 5 --deblock -1:-1 --aq-strength 0.80 --psy-rd 1.8 --rdoq-level 2 --psy-rdoq 1.5 --ssim-rd --max-merge 4 --ref 4 --me 3 --subme 5 --merange 32 --no-early-skip --rect --amp --limit-modes --min-keyint 1 --keyint 240 --rc-lookahead 70 --bframes 8 --crf 20 --colormatrix bt709 -o "%FILE%.mkv"
+vspipe --y4m "%1" - | x265 --y4m - --profile main10 --pools + --pmode --ctu 32 --min-cu-size 8 --tu-intra-depth 3 --tu-inter-depth 2 --limit-tu 2 --limit-refs 0 --rd 5 --deblock -1:-1 --aq-strength 0.80 --psy-rd 1.8 --rdoq-level 2 --psy-rdoq 1.5 --ssim-rd --max-merge 4 --ref 4 --me 3 --subme 5 --merange 32 --rect --amp --limit-modes --min-keyint 1 --keyint 240 --rc-lookahead 70 --bframes 8 --crf 20 --colormatrix bt709 -o "%FILE%.mkv"
 
 pause
